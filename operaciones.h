d42cf727410d18a3d28427f8bfb82ce783010479
estuardo-02 <109821591+estuardo-02@users.noreply.github.com>
2023-03-22 21:36:10 -0600

    Create operaciones.h

---
+++ operaciones.h
@@ -0,0 +1,9 @@
+#ifndef OPP_H
+#define OPP_H
+
+float add(float a, float b);
+float sub(float a, float b);
+float mult(float a, float b);
+float divs(float a, float b);
+float pi();
+#endif /*OPP_H*/
