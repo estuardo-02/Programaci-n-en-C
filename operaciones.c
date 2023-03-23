26e996f48b09147055e6cdedf275296968c6cfbf
estuardo-02 <109821591+estuardo-02@users.noreply.github.com>
2023-03-22 21:35:51 -0600

    Create operaciones.c

---
+++ operaciones.c
@@ -0,0 +1,23 @@
+#include "operaciones.h"
+float add(float a, float b)
+{
+    return a + b; 
+}
+float sub(float a, float b)
+{
+    return a - b;
+}
+float mult(float a, float b)
+{
+    return a*b;
+}
+
+float divs(float a, float b)
+{
+    return a/b;
+}
+float pi()
+{
+    float pi_val = 3.141592;
+    return pi_val;
+}
